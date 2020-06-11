3458ac5abe6bab5b4d40b8884f6f34431e123cee
kkumari <kusum.kumarisjce@gmail.com>
2020-06-11 21:38:16 +0530

    added test.py file

---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np
+import mcvoronoi 
+
+
+points = np.random.rand(10, 2)  # a numpy array of 10 input co-ordinates
+lat_lon_area, mean_percentage_error = mcvoronoi.voronoi_area(points, voronoi_plot_enabled=True, NUM_COLORS=5) #calling function voronoi_area from source.py
