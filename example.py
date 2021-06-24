eebb40c3230eadb44d68bc0435199567c28d0a2f
NISHANT KUMAR <abc.nishant007@gmail.com>
2021-06-24 22:15:28 +0800

    some housekeeping

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
 import numpy as np
-import mcvoronoi 
+
+# this is just a quickfix so that we can run without the pip install
+try:
+    from mcvoronoi import voronoi_area  # if pip installed
+except:
+    from source import voronoi_area               # if not pip installed, and using the git version
 
 
 points = np.random.rand(10, 2)  # a numpy array of 10 input co-ordinates
-lat_lon_area, mean_percentage_error = mcvoronoi.voronoi_area(points,voronoi_plot_enabled=True, NUM_COLORS=5, metric='manhattan')
+lat_lon_area, mean_percentage_error = voronoi_area(points,voronoi_plot_enabled=True, NUM_COLORS=5, metric='manhattan')
--- source.py
+++ source.py
@@ -1,4 +1,4 @@
-def voronoi_area(input_coordinates, number_of_iterations=50, metric="euclidean",number_of_trials_per_iteration=10000, error_plot_enabled=True, voronoi_plot_enabled=False, sizeOfMarker=0.5, NUM_COLORS=20):
+def voronoi_area(input_coordinates, number_of_iterations=50, metric="euclidean",number_of_trials_per_iteration=1000, error_plot_enabled=True, voronoi_plot_enabled=False, sizeOfMarker=0.5, NUM_COLORS=20):
   
     import matplotlib as mpl
     import matplotlib.cm as cm
@@ -127,6 +127,3 @@ def voronoi_area(input_coordinates, number_of_iterations=50, metric="euclidean",
         return lat_lon_area, error_record[-1]
 
 
-import numpy as np
-points = np.random.rand(10, 2)  # a numpy array of 10 input co-ordinates
-lat_lon_area, mean_percentage_error = voronoi_area(points, voronoi_plot_enabled=True, NUM_COLORS=5)
